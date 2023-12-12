55c46e0ae8bc307bcc7ad14a8ff5e8253873fb3c
Pedro Barros <pedro.hcb@live.com>
2023-12-12 01:26:03 -0300

    Código usado para o slide de REACT RIA

---
+++ reactando/src/Components/FirstComp.jsx
@@ -0,0 +1,19 @@
+import './FirstComp.css'
+
+export default function FirstComp() {
+  
+  return (
+    <>
+      <h1>Texto para maiúsculo abaixo:</h1>
+    </>
+  )
+}
+
+// export default function FirstComp({texto, outra}) {
+//     return (
+//       <>
+//         <h1>Texto para maiúsculo abaixo:</h1>
+//         <h2>{texto.toUpperCase()}</h2>
+//       </>
+//     )
+// }
