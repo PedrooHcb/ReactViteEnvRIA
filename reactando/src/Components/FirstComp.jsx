1bcf601c9b0d71a670b575788c52d323b14a690b
Pedro Barros <pedro.hcb@live.com>
2026-02-01 00:33:27 -0300

    Remove local to liberate space on C:/

--- reactando/src/Components/FirstComp.jsx
+++ reactando/src/Components/FirstComp.jsx
@@ -1,19 +1,19 @@
 import './FirstComp.css'
 
-export default function FirstComp() {
+// export default function FirstComp() {
   
-  return (
-    <>
-      <h1>Texto para maiúsculo abaixo:</h1>
-    </>
-  )
-}
-
-// export default function FirstComp({texto, outra}) {
-//     return (
-//       <>
-//         <h1>Texto para maiúsculo abaixo:</h1>
-//         <h2>{texto.toUpperCase()}</h2>
-//       </>
-//     )
+//   return (
+//     <>
+//       <h1>Texto para maiúsculo abaixo:</h1>
+//     </>
+//   )
 // }
+
+export default function FirstComp({texto, outra}) {
+    return (
+      <>
+        <h1>Texto para maiúsculo abaixo:</h1>
+        <h2>{texto.toUpperCase()}</h2>
+      </>
+    )
+}
